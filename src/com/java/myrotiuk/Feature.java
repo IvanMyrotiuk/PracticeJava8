d86b4220d7ad983f9085413b076278b98cc049b8
IvanMyrotiuk <ivan.myrotiuk@gmail.com>
2016-10-30 19:50:02 +0200

    Feature#1

--- src/com/java/myrotiuk/Feature.java
+++ src/com/java/myrotiuk/Feature.java
@@ -3,8 +3,5 @@ package com.java.myrotiuk;
 /**
  * Created by Ivan on 30.10.2016. All rights reserved.
  */
-public class AAAAAA {
-    public static void main(String[] args) {
-
-    }
+public class Feature {
 }
